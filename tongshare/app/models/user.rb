a99c34a6fc5860c5cd6b9d1d60e998d1bb1135e6
lywander <lywander@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-02-08 15:11:01 +0000

    Try branch. Just a try

--- tongshare/app/models/user.rb
+++ tongshare/app/models/user.rb
@@ -2,7 +2,19 @@
 # user_sharing is the sharing the shares to this user.
 # All sharing shares to this user should also include
 # group_sharing that related to this user
+
+#by Wander: add columns for devise. NOTE: do not use 'email' in this table, use that in the user_extra instead. Will delete 'email' column in the future. 
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :confirmable, :lockable and :timeoutable
+  devise :database_authenticatable, 
+         :token_authenticatable, #for mobile auto-login
+         :recoverable, :rememberable, :trackable, :validatable
+         #no registerable, we will rewrite it.
+         #no confirmable, we will rewrite it
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :remember_me
   has_many :user_identifier, :dependent => :destroy
   has_many :group, :foreign_key => "creator_id", :dependent => :destroy
   has_many :membership, :dependent => :destroy
--- tongshare/config/routes.rb
+++ tongshare/config/routes.rb
@@ -1,4 +1,8 @@
 Tongshare::Application.routes.draw do
+  devise_for :users
+
+  get "home/index"
+
   get "temp/view"
 
   resources :users
@@ -56,7 +60,7 @@ Tongshare::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "home#index"
 
   # See how all your routes lay out with "rake routes"
 
