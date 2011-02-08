5f49e20964c648c5892bc5e9dea2424630a1f087
liyuqian79 <liyuqian79@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-02-08 15:54:32 +0000

    应Wander要求改进了注释，求review和具体的意见

--- trunk/tongshare/app/models/user.rb
+++ trunk/tongshare/app/models/user.rb
@@ -1,7 +1,12 @@
 # Note! sharing is the sharing created by user.
-# user_sharing is the sharing the shares to this user.
+# user_sharing is the sharing that shares to this user.
 # All sharing shares to this user should also include
 # group_sharing that related to this user
+# 注意！this.sharings只是由该user创建的sharing（分享）。
+# this.user_sharings只是专门针对该user的sharing。
+# 所有和该用户相关的分享（即分享给用户的事件）还应该包含
+# 该用户所在的群组相关的this.groups.group_sharings。
+#
 class User < ActiveRecord::Base
   has_many :user_identifier, :dependent => :destroy
   has_many :group, :foreign_key => "creator_id", :dependent => :destroy
