70a2398448a3ac1efaa88fa3bfbbb6291e1560e4
liyuqian79 <liyuqian79@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-02-14 06:41:06 +0000

    稍微修改了一下thuxls测试文件。另外，大家原来提交的时候都rake test过吗？为什么我这里rake test全是错？

--- tongshare/test/functional/thuxls_test.rb
+++ tongshare/test/functional/thuxls_test.rb
@@ -2,11 +2,9 @@ require 'thuxls'
 
 $KCODE = 'u'
 
-if ($*.size > 0)
-  table = xls2table($*[0])
-  course_set = Course.parse_table(table)
-  pp course_set
-else
-  puts "You must provide a filename."
-end
+filename = "tmp/lc.xls"
+
+table = xls2table(filename)
+course_set = Course.parse_table(table)
+pp course_set
 
