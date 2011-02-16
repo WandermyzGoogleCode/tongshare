9b70e6138bc734ae529d63952930350a20ce76f0
liyuqian79 <liyuqian79@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-02-16 05:10:42 +0000

    进行了性能测试，目前发现一处瓶颈(now += 1.day)并对其进行了改正(now += 3600*24)，其它瓶颈正在改进中

--- tongshare/db/schema.rb
+++ tongshare/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110214084103) do
+ActiveRecord::Schema.define(:version => 20110215155715) do
 
   create_table "acceptances", :force => true do |t|
     t.integer  "event_id"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(:version => 20110214084103) do
   create_table "consumer_tokens", :force => true do |t|
     t.integer  "user_id"
     t.string   "type",       :limit => 30
-    t.string   "token",      :limit => 1024
+    t.string   "token",      :limit => 128
     t.string   "secret"
     t.datetime "created_at"
     t.datetime "updated_at"
