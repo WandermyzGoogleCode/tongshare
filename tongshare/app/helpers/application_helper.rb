00705f78079f95d021244ebb9f8b1288f356e706
lywander@gmail.com <lywander@gmail.com@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-02-17 17:56:17 +0000

    外观（窘死一个算一个）

--- tongshare/app/helpers/application_helper.rb
+++ tongshare/app/helpers/application_helper.rb
@@ -13,4 +13,47 @@ module ApplicationHelper
 
     html.html_safe
   end
+
+  def content_box(title_strong = nil, title = nil, border = false, &block)
+    title_str = ""
+    title_str += "<span>#{title_strong}</span>" unless title_strong.blank?
+    title_str += title unless title.blank?
+    title_str = "<h2>#{title_str}</h2>" unless title_str.blank?
+    if border
+      content = <<HTML
+    		<div class="box">
+					<div class="border-right">
+						<div class="border-bot">
+							<div class="border-left">
+								<div class="left-top-corner">
+									<div class="right-top-corner">
+										<div class="inner">
+											#{title_str}
+											#{capture(&block)}
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+HTML
+    else
+      content = <<HTML
+        <div class="inside">
+					#{title_str}
+					#{capture(&block)}
+				</div>
+HTML
+    end
+    content.html_safe
+  end
+
+  def styled_button(text, url, submit = false)
+    content = <<HTML
+    <div class="wrapper"><a href="#{url}" class="link1"><span><span>#{text}</span></span></a></div>
+HTML
+    content.html_safe
+  end
+
 end
