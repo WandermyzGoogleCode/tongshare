9be187f484727e9b848fe675134d3484e4a3a5f1
liyuqian79 <liyuqian79@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-02-17 17:10:13 +0000

    求完善confirm以导入课表

---
+++ trunk/tongshare/app/controllers/auth_controller.rb
@@ -0,0 +1,21 @@
+require 'base64'
+require 'digest/sha2'
+
+class AuthController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  
+  SECRET = "this is a secret" # should be the same with ThuAuth
+
+  def confirm
+    name = params[:name]
+    username = params[:username] # 学号
+    hash_value = params[:hash]
+    data = Base64.decode64(params[:data])
+    @output = "params not valid"
+    return unless name && username && hash_value && data
+    my_hash = (Digest::SHA2.new << (username + name + SECRET)).to_s
+    @output = "hash not valid"
+    return unless my_hash == hash_value
+    @output = "accepted"
+  end
+end
