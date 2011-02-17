c112d9d67a6cb369408c2ef195eae07336fcba89
lywander@gmail.com <lywander@gmail.com@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-02-17 13:05:41 +0000

    手机版页面

--- trunk/tongshare/app/controllers/home_controller.rb
+++ trunk/tongshare/app/controllers/home_controller.rb
@@ -3,8 +3,7 @@
 
 class HomeController < ApplicationController
   def index
-
-
+    logger.debug "UserAgent: " + request.env["HTTP_USER_AGENT"]
 
     if user_signed_in?
       flash[:notice] = notice
