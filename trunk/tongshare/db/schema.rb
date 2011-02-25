bd4c54bd9a6d2e012c32805c7d60c78585a19fd2
liyuqian79 <liyuqian79@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-02-25 07:32:48 +0000

    auth_token and its automatic notice are done. Please test and check it. I18n is not implemented so the Chinese notice message is in the source file.

--- trunk/tongshare/db/schema.rb
+++ trunk/tongshare/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110219113536) do
+ActiveRecord::Schema.define(:version => 20110225043205) do
 
   create_table "acceptances", :force => true do |t|
     t.integer  "event_id"
@@ -23,6 +23,14 @@ ActiveRecord::Schema.define(:version => 20110219113536) do
   create_table "admin_extras", :force => true do |t|
     t.integer  "user_id"
     t.boolean  "admin_enabled"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "bookmarks", :force => true do |t|
+    t.integer  "user_id"
+    t.string   "mark"
+    t.integer  "count"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -150,8 +158,10 @@ ActiveRecord::Schema.define(:version => 20110219113536) do
     t.string   "confirmation_token"
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
+    t.string   "authentication_token"
   end
 
+  add_index "users", ["authentication_token"], :name => "index_users_on_authentication_token", :unique => true
   add_index "users", ["confirmation_token"], :name => "index_users_on_confirmation_token", :unique => true
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
