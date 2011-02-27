0ae9390c1636c87b7b544905a680250f3e046646
cbmixx@gmail.com <cbmixx@gmail.com@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-02-27 07:32:38 +0000

    remove unmoded part from branch

--- thuauth/test/functional/thuauth_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ThuauthControllerTest < ActionController::TestCase
-  test "should get auth_with_xls_and_get_name" do
-    get :auth_with_xls_and_get_name
-    assert_response :success
-  end
-
-end
