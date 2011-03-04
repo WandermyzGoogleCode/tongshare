a684e7b9a969560462ea2fde0f0891be4c9492a6
lywander@gmail.com <lywander@gmail.com@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-03-04 03:15:58 +0000

    修改若干db migration

--- tongshare/db/migrate/20110303053805_change_renren_id_format.rb
+++ tongshare/db/migrate/20110303053805_change_renren_id_format.rb
@@ -1,10 +1,19 @@
 class ChangeRenrenIdFormat < ActiveRecord::Migration
+  class UserExtra < ActiveRecord::Base
+    attr_accessible :renren_id
+  end
+
   def self.up
-    execute("UPDATE user_extras SET renren_id = 'id:'||renren_id WHERE NOT (renren_id LIKE 'domain:%') ")
-    # || means concat
+    UserExtra.find(:all, :conditions => "NOT (renren_id LIKE 'domain:%')").each do |ue|
+      ue.renren_id = 'id:' + ue.renren_id
+      ue.save!
+    end
   end
 
   def self.down
-    execute("UPDATE user_extras SET renren_id = SUBSTR(renren_id, 4) WHERE renren_id LIKE 'id:%'")
+    UserExtra.find(:all, :conditions => "renren_id LIKE 'id:%'").each do |ue|
+      ue.renren_id = ue.renren_id[3..-1]
+      ue.save!
+    end
   end
 end
