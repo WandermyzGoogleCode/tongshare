6de9331e9de9a2fa0b15d1d6e4e1bd5c9c9d9757
liyuqian79 <liyuqian79@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-03-11 09:45:56 +0000

    貌似把站点specific的thuauth搞定了，不过因为一会要吃饭，所以还是吃完饭再回来部署吧

--- trunk/thuauth/app/controllers/thuauth_controller.rb
+++ trunk/thuauth/app/controllers/thuauth_controller.rb
@@ -21,6 +21,13 @@ class ThuauthController < ApplicationController
     #    Thread.exclusive do
     @password = params[:password]
     @username = params[:username]
+    @post_url = params[:post_url] || TONGSHARE_AUTH_CONFIRM_URL
+    @post_hash = params[:post_hash]
+    puts "TEST A: " + @post_url
+    if (!@post_hash || @post_hash != (Digest::SHA2.new << @post_url + SECRET).to_s)
+      @post_url = TONGSHARE_AUTH_CONFIRM_URL
+    end
+    puts "TEST B: " + @post_url
     @redirect_to = params[:redirect_to]
     @aes = params[:aes]
     begin
@@ -42,7 +49,7 @@ class ThuauthController < ApplicationController
           data = ""
         end
         hash_value = (Digest::SHA2.new << @username + name + SECRET).to_s
-        response = Net::HTTP.post_form(URI.parse(TONGSHARE_AUTH_CONFIRM_URL),
+        response = Net::HTTP.post_form(URI.parse(@post_url),
           {:username => @username, :name => name, :hash => hash_value, :data => data}
         )
         #          headers = {'Content-Type' => 'application/octet-stream'}
