b181e98f654eb3eb3f1262a0e054b56c73c8b9d0
liyuqian79 <liyuqian79@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-03-12 08:02:55 +0000

    fixed deploy.rb about the sym link

--- thuauth/config/deploy.rb
+++ thuauth/config/deploy.rb
@@ -51,6 +51,6 @@ after "deploy:update_code", :link_and_bundle_install
 desc "install the necessary prerequisites"
 task :link_and_bundle_install, :roles => :app do
 #  run "cd #{release_path} && bundle install"
-  run "ln -s /var/www/thuauth/shared/environments/production.rb #{release_path}/config/environments/production.rb && \
+  run "ln -s /var/www/thuauth/shared/environments #{release_path}/config/environments && \
     cd #{release_path} && bundle install"
 end
