412a8e1e621aefc7e228b0c55524bdfefbd8d9bd
cbmixx@gmail.com <cbmixx@gmail.com@c2c84ded-d1d5-93fd-fce7-e2d88d762596>
2011-03-30 07:56:37 +0000

    basic group function added

--- tongshare/app/helpers/groups_helper.rb
+++ tongshare/app/helpers/groups_helper.rb
@@ -1,2 +1,10 @@
 module GroupsHelper
+  def add_group(name, creator_id = current_user_id, privacy = Group::PRIVACY_PRIVATE, extra_info = nil)
+    group = Group.new(:creator_id => creator_id, :name => name, :privacy => privacy, :extra_info => extra_info)
+    group.save
+  end
+
+  def query_group_via_name_and_creator_id(name, creator_id)
+    Group.where(:name => name, :creator_id => creator_id).first
+  end
 end
